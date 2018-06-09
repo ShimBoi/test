3b7565f052aee0dacf0effc5c3df793e8c41c8cf
unknown <jayshim0725@gmail.com>
2018-06-09 11:04:19 -0700

    created AI and creepy bunny. Made door-ish thing

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class Enemy : MonoBehaviour {
+	public Transform destination2;
+	public Transform destination;
+	public Transform player;
+	public NavMeshAgent AI;
+	public float distanceThreshold = 2;
+	public int place1 = 0;
+	// Use this for initialization
+
+	void Start () {
+		AI = GetComponent<NavMeshAgent>();
+		AI.SetDestination (destination.position);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Debug.Log (AI.remainingDistance);
+		if (AI.remainingDistance < distanceThreshold ) {
+			if (place1 == 0)
+			{
+				Debug.Log ("Going place 2");
+				place1 = 1;
+				AI.SetDestination (destination.position);	
+			}else if (place1 == 1){
+				Debug.Log ("Going place 1");
+				place1 = 0;
+				AI.SetDestination (destination2.position);	
+			}
+			if(place1 == -1)
+			{
+				AI.SetDestination (player.position);	
+			}
+		}
+
+
+	}
+	void OnTriggerEnter(Collider col)
+	{
+		if (col.gameObject.CompareTag ("Player")) {
+			Debug.Log ("attack");
+			player = col.transform;
+			place1 = -1;
+		}
+	}
+}
--- Assets/Scripts/GunControl.cs
+++ Assets/Scripts/GunControl.cs
@@ -5,17 +5,17 @@ using UnityEngine;
 public class GunControl : MonoBehaviour {
 	public GameObject bulletPrefab;
 	public Transform barrelTip;
-
+	public CursorLockMode wantedMode;
 	// Use this for initialization
 	void Start () {
-		
+		Cursor.lockState = wantedMode;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKey (KeyCode.Mouse0)) {
 			GameObject newBullet = Instantiate (bulletPrefab, barrelTip.position, barrelTip.rotation);
-			newBullet.GetComponent<Rigidbody> ().AddRelativeForce (Vector3.forward * 1000);
+			newBullet.GetComponent<Rigidbody> ().AddRelativeForce (Vector3.forward * 1500);
 		
 		}
 
