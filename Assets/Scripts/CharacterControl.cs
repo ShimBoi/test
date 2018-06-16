dc252d0ee38b5fca63b7c7aead1227a91ea153ad
unknown <jayshim0725@gmail.com>
2018-06-16 11:07:29 -0700

    created health bar and slider. Used text and UI. Added friction so character will not slide. added losing screen and property

--- Assets/Scripts/CharacterControl.cs
+++ Assets/Scripts/CharacterControl.cs
@@ -1,14 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class CharacterControl : MonoBehaviour {
 	Rigidbody rigidbody;
 	public float moveSpeed ;
-
-
-
+	public float jumpSpeed = 150;
+	public int health = 100;
+	public Slider healthBar;
+	public Text healthText;
+	public Text DieScreen;
+	string maxHelath;
 	bool onGround = false;
+
+	public int Health {
+		get{return health;}
+		set{
+			health = value;
+			if(health>maxHelath){
+				health = maxHelath;
+				return
+			}
+			healthBar.value = health;
+			Debug.Log (health);
+			healthText.text = health + "/"+maxHelath;
+			if (health <= 0) {
+				DieScreen.text = "You Died";
+				healthText.text = "0/" + maxHelath;
+			}
+		}
+
+	}
 
 	// Use this for initialization
 	void Start () {
@@ -16,31 +38,41 @@ public class CharacterControl : MonoBehaviour {
 		// Make the rigid body not change rotation
 		if (rigidbody)
 			rigidbody.freezeRotation = true;
+		maxHelath = ""+health;
+		healthBar.maxValue = health;
+		healthText.text= health +"/"+maxHelath ;
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKey(KeyCode.L)) {
-			Debug.Log ("hit");
-		}
-		if(Input.GetKey(KeyCode.W)){
-			rigidbody.AddRelativeForce (Vector3.forward * moveSpeed);
-		}
-		if(Input.GetKey(KeyCode.S)){
-			rigidbody.AddRelativeForce (Vector3.back * moveSpeed);
-		}
-		if(Input.GetKey(KeyCode.A)){
-			rigidbody.AddRelativeForce (Vector3.left* moveSpeed);
-		}
-		if(Input.GetKey(KeyCode.D)){
-			rigidbody.AddRelativeForce (Vector3.right* moveSpeed);
-		}
-		if(Input.GetKey(KeyCode.Space) && onGround){
-			rigidbody.AddRelativeForce (Vector3.up* 100);
-			onGround = false;
+		if(Input.GetKey(KeyCode.P)){
+			Health += 2;
 		}
 
+		if(onGround == true){
+			if(Input.GetKey(KeyCode.W)){
+				rigidbody.AddRelativeForce (Vector3.forward * moveSpeed);
+			}
+			if(Input.GetKey(KeyCode.S)){
+				rigidbody.AddRelativeForce (Vector3.back * moveSpeed);
+			}
+			if(Input.GetKey(KeyCode.A)){
+				rigidbody.AddRelativeForce (Vector3.left* moveSpeed);
+			}
+			if(Input.GetKey(KeyCode.D)){
+				rigidbody.AddRelativeForce (Vector3.right* moveSpeed);
+			}
+			if(Input.GetKey(KeyCode.Space) && onGround){
+				rigidbody.AddRelativeForce (Vector3.up* jumpSpeed);
+				onGround = false;
+			}
+				
+		}
+		if (onGround == false){
+			
+		}
+			
 
 	}
 
@@ -61,6 +93,13 @@ public class CharacterControl : MonoBehaviour {
 			Debug.Log ("keep hitting");
 			transform.position =  new Vector3 (90f, 90f);
 		} 
+	}
+	void OnCollisionEnter(Collision col){
+		if (col.gameObject.CompareTag ("rabbit")) {
+			Health -= 3;
+
+		}
+
 	}
 
 }
